4a8fe6589c35b6d703471049a77c63c4e3718720
Imogen <me@imogen.tech>
2022-01-13 16:57:18 +0100

    letter mapping

--- sender.py
+++ sender.py
@@ -1,15 +1,35 @@
 from time import sleep
 import httpx
 
+letter_map = [
+    ".", ",", "-", "v", "l", "m", "j", "w",
+    "²", "µ", "f", "^", ">", "´", "+", "1",
+
+    "2", "3", "4", "5", "6", "7", "8", "9",
+    "0", "E", "£", "B", "F", "P", "S", "Z",
+
+    "V", "&", "Y", "A", "T", "L", "$", "R",
+    "*", "C", '"', "D", "?", "N", "I", "U",
+
+    ")", "W", "_", "=", ";", ":", "M", "'",
+    "H", ")", "K", "/", "O", "!", "X", "§",
+
+    "Q", "J", "%", "³", "G", "°", "Ü", "`",
+    "Ö", ">", "Ä", "#", "t", "x", "q", "ß",
+
+    "ü", "ö", "ä", "<", "k", "p", "h", "o",
+    "g", "n", "r", "s", "e", "a", "i", "b",
+
+    "u", "d", "p", "z",
+] 
 
 try:
     print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "Online"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4aad"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4bad"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4cad"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4dad"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4ead"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4fad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "44ad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "45ad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "46ad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "47ad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "48ad"}))
     print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "Offline"}))
 except Exception as e:
     print(e)
