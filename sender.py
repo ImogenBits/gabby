a6805c5c5358ece7080d73cdf7454fd4c36fc656
Imogen <me@imogen.tech>
2022-01-13 16:22:05 +0100

    wah

--- sender.py
+++ sender.py
@@ -1,12 +1,15 @@
 from time import sleep
 import httpx
 
-#print(httpx.post("http://192.168.178.151:80/SerialSend", data={"data": "Online"}))
-#sleep(3)
 
 try:
     print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "Online"}))
-    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4e6d"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4aad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4bad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4cad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4dad"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4ead"}))
+    print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "4fad"}))
     print(httpx.post("http://192.168.178.25:80/SerialSend", data={"data": "Offline"}))
 except Exception as e:
     print(e)
